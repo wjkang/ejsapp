be8361560502a908de8d547edf4ddc3a3e5b46a5
Administrator <977865769@qq.com>
2016-03-01 11:19:01 +0800

    ejs

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/3/1.
+ */
+function initConfig(){
+    var configInfo={
+        mysqlConfig:{
+            'host':'192.0.0.1',
+            'user':'root',
+            'password':'977865769',
+            'datebase':'wjkblog'
+        }
+    };
+    return configInfo;
+}
+exports.initConfig=initConfig;
