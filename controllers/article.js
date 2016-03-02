d3ee575eb9d58a9928eaa2e7e2cdeea32ef571d7
Administrator <977865769@qq.com>
2016-03-02 11:55:08 +0800

    ejs

--- controllers/article.js
+++ controllers/article.js
@@ -2,3 +2,12 @@
  * Created by Administrator on 2016/3/1.
  */
 var Article=require("../proxy/article");
+
+exports.GetArticles=function(req,res){
+    Article.GetArticles(function(err,rows){
+        if(err){
+            res.send(err);
+        }
+        res.render("index",{rows:rows});
+    });
+};
